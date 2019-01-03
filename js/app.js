75e00cb726fbd243dcec7fff2b5dad1c03912053
Spencer Dant <sdant@Spencers-MacBook-Pro.local>
2019-01-03 12:46:15 -0600

    corrected intervals, barebones styling

--- js/app.js
+++ js/app.js
@@ -21,37 +21,72 @@ zatchi = {
 	age: 0
 		// increase age by 1 every 10 minutes
 }
-	
+let hungerText = $('#hunger');
+let sleepinessText = $('#sleepiness');
+let boredomText = $('#boredom');
+let ageText = $('#age');
+
 let newHunger = zatchi.hunger;
 let newSleepiness = zatchi.sleepiness;
 let newBoredom = zatchi.boredom;
 let newAge = zatchi.age;
 
-// newHunger.setInterval(zatchiHunger, 120000);
-function zatchiHunger() {
-	newHunger = setInterval(zatchi.hunger++, 120000);
-	if (zatchi.hunger === 10) {
-		clearInterval(newHunger);
+const game = {
+	startGame() {
+		// name your pet!
+		// display (pet's name) + (statistics)
+		// call all methods
+		this.zatchiHunger();
+		this.zatchiSleepiness();
+		this.zatchiBoredom();
+		this.zatchiAge();
+	},
+	// newHunger.setInterval(zatchiHunger, 120000);
+	zatchiHunger() {
+		newHunger = setInterval(function () {
+			zatchi.hunger++;
+			if (zatchi.hunger === 10) {
+				clearInterval(newHunger);
+				alert('Your pet died!');
+			} else {
+				hungerText.text(`HUNGER: ${zatchi.hunger}`);
+			}
+		}, 20000)
+	},
+
+	zatchiSleepiness() {
+		newSleepiness = setInterval(function () {
+			zatchi.sleepiness++;
+			if (zatchi.sleepiness === 10) {
+				clearInterval(newSleepiness);
+				alert('Your pet died!');
+			} else {
+				sleepinessText.text(`SLEEPINESS: ${zatchi.sleepiness}`);
+			}
+		}, 40000)
+	},
+
+	zatchiBoredom() {
+		newBoredom = setInterval(function () {
+			zatchi.boredom++;
+			if (zatchi.boredom === 10) {
+				clearInterval(newBoredom);
+				alert('Your pet died!');
+			} else {
+				boredomText.text(`BOREDOM: ${zatchi.boredom}`)
+			}
+		}, 100000)
+	},
+
+	zatchiAge() {
+		newAge = setInterval(function () {
+			zatchi.age++;
+			ageText.text(`AGE: ${zatchi.age}`)
+		}, 600000)
 	}
 }
 
-function zatchiSleepiness() {
-	newSleepiness = setInterval(zatchi.sleepiness++, 180000);
-	if (zatchi.sleepiness === 10) {
-		clearInterval(newSleepiness);
-	}
-}
-
-function zatchiBoredom() {
-	newBoredom = setInterval(zatchi.boredom++, 360000);
-	if (zatchi.boredom === 10) {
-		clearInterval(newBoredom);
-	}
-}
-
-function zatchiAge() {
-	newAge = setInterval(zatchi.age++, 600000);
-}
+game.startGame();
 
 
 
